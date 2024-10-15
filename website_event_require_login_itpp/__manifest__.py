9ae15fbca95a9d14a05afd1ae28d42f2ad14eb37
Eugene Molotov <molotov@it-projects.info>
2024-10-15 12:54:27 +0500

    [ADD] website_event_require_login_itpp
    Co-authored-by: Ivan Yelizariev <yelizariev@users.noreply.github.com>
    Co-authored-by: Artem Rafailov <Ommo73@users.noreply.github.com>
    Co-authored-by: Eugene Molotov <em230418@users.noreply.github.com>
    Co-authored-by: Victor Bykov <BykovVik@users.noreply.github.com>

---
+++ website_event_require_login_itpp/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": """No tickets without Email""",
+    "summary": """User registration at your portal is always a plus for marketing""",
+    "category": "Marketing",
+    "images": ["images/banner.jpg"],
+    "version": "14.0.1.0.0",
+    "application": False,
+    "author": "IT-Projects LLC",
+    "support": "apps@it-projects.info",
+    "website": "https://github.com/it-projects-llc/website-addons",
+    "license": "LGPL-3",
+    "depends": ["website_event"],
+    "data": ["views/website_event_sale_templates.xml"],
+    "qweb": [],
+    "demo": [],
+}

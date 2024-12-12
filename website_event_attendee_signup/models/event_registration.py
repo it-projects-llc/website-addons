2183394a99e8b0e036ac383db051b77ec7e5c844
Eugene Molotov <molotov@it-projects.info>
2024-12-12 15:40:46 +0500

    [FIX] website_event_attendee_signup: готовим пользователя к регистрации до отправки почты с шаблоном

--- website_event_attendee_signup/models/event_registration.py
+++ website_event_attendee_signup/models/event_registration.py
@@ -1,24 +1,41 @@
-from odoo import api, models
+from odoo import models
 
 
 class EventRegistration(models.Model):
     _inherit = "event.registration"
 
-    @api.model
-    def create(self, vals):
-        res = super().create(vals)
+    def _update_attendee_partner_id(self, vals):
+        vals = super()._update_attendee_partner_id(vals)
+        if not vals.get("attendee_partner_id"):
+            return vals
 
-        if res.event_id.attendee_signup and res.attendee_partner_id:
-            login = res.attendee_partner_id.email
-            user = self.env["res.users"].search([("login", "=ilike", login)])
-            if not user:
-                user = (
-                    self.env["res.users"]
-                    .sudo()
-                    ._signup_create_user(
-                        {"login": login, "partner_id": res.attendee_partner_id.id}
-                    )
-                )
-                user.partner_id.signup_prepare()
+        if not vals.get("event_id"):
+            return vals
 
-        return res
+        Partner = self.env["res.partner"].sudo()
+        Users = self.env["res.users"].sudo()
+        Events = self.env["event.event"].sudo()
+
+        event = Events.browse(vals["event_id"])
+        if not event.attendee_signup:
+            return vals
+
+        attendee_partner = Partner.browse(vals["attendee_partner_id"])
+        if attendee_partner.user_ids:
+            # already has user
+            return vals
+
+        login = attendee_partner.email
+        if not login:
+            return vals
+
+        user = Users.search([("login", "=ilike", login)])
+        if user:
+            # already has user with given address
+            return vals
+
+        user = Users._signup_create_user(
+            {"login": login, "partner_id": attendee_partner.id}
+        )
+        user.partner_id.signup_prepare()
+        return vals

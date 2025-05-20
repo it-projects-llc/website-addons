51d37a77486aece6ccc2102a24a77073c03781d6
Eugene Molotov <molotov@it-projects.info>
2025-05-20 10:50:02 +0500

    [REM] portal_event_tickets: убираем неработающую фильтрацию при upgrade ticket

--- portal_event_tickets/models/event.py
+++ portal_event_tickets/models/event.py
@@ -1,5 +1,4 @@
 from odoo import fields, models
-from odoo.http import request
 
 
 class Event(models.Model):
@@ -22,19 +21,3 @@ class Event(models.Model):
         help="Attendee can change ticket to new ticket or products",
         default=True,
     )
-
-    def check_partner_for_new_ticket(self, partner_id):
-        registration = self.partner_is_participating(partner_id)
-        if registration:
-            ticket_order_lines = registration.mapped("sale_order_line_id")
-
-            if request.website.sale_get_order():
-                cart_refund_lines = request.website.sale_get_order().order_line.mapped(
-                    lambda r: r.refund_source_line_id
-                )
-                # all refund lines must be in ticket_order_lines
-                if all([line in cart_refund_lines for line in ticket_order_lines]):
-                    # False means no errors
-                    return False
-
-        return super().check_partner_for_new_ticket(partner_id)
